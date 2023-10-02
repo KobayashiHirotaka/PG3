c101d0a265c4cb89d2637ed1a344f59e8ea29959
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-10-02 12:52:52 +0900

    01_02完了

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
 ﻿#include <stdio.h>
 
+int Normal(int n)
+{
+	return (n * 1072);
+}
+
 int Recursive(int n)
 {
 	if (n <= 1)
 	{
-		return 100;
+		return (100);
 	}
 
-	return (Recursive(n - 1) * 2 -50);
+	return (Recursive(n - 1) * 2 - 50);
 }
 
 
 int main(void)
 {
-	int num = 4;
-	int result = 0;
+	int time = 9;
+	int result[2] = { 0,0 };
 
-	result = Recursive(num);
+	result[0] = Normal(time);
+	result[1] = Recursive(time);
 
-	for (int i = 0; i < 24; i++)
-	{
-		printf("%d時間働くと%d\n", i, result);
-	}
+	printf("一般的：%d時間働くと%d\n", time, result[0]);
+	printf("再帰的：%d時間働くと%d\n", time, result[1]);
 
 	return 0;
 }
