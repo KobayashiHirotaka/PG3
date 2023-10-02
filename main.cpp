1599bc702f4a84140b69843d743b52390744699f
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-10-02 11:08:15 +0900

    01_01完成

---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+template <typename T>
+
+T Min(T a, T b)
+{
+	if (a < b)
+	{
+		return a;
+	}
+}
+
+template<>
+char Min<char>(char a, char b)
+{
+	return printf("数字以外は代入出来ません");
+}
+
+int main(void)
+{
+	char chr1 = 'a';
+	char chr2 = 'b';
+
+	printf("%d\n", Min<int>(200, 500));
+	printf("%f\n", Min<float>(1.0f, 3.0f));
+	printf("%f\n", Min<double>(100.333, 80.333));
+	printf("%c\n", Min<char>(chr1, chr2));
+
+	return 0;
+}
