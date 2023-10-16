f6217efca5cf223de63eff121ea98b1a6b004667
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-10-16 12:38:20 +0900

    01_03課題完了

---
+++ main.cpp
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <time.h>
+#include <stdlib.h>
+
+typedef void (*PFunc)(int, char *);
+
+void SetTimeout(int second)
+{
+    Sleep(second * 1000);
+}
+
+void DiceRoll(PFunc p)
+{
+    srand(time(NULL));
+    int result = rand() % 6 + 1;
+
+    char guess[10];
+    printf("奇数か偶数かを入力してください : ");
+    scanf_s("%s", guess, sizeof(guess));
+
+    p(result, guess);
+}
+
+void CheckResult(int result, char* guess)
+{
+    int diceResult = (result % 2 == 1);
+    int isCorrect = 0;
+
+    if ((diceResult && strcmp(guess, "奇数") == 0) || (!diceResult && strcmp(guess, "偶数") == 0))
+    {
+        isCorrect = 1;
+
+    } else {
+        isCorrect = 0;
+    }
+
+    SetTimeout(3);
+
+    printf("結果は %s です。\n", isCorrect ? "正解" : "不正解");
+    printf("出た目は %d です。\n", result);
+}
+
+int main() 
+{
+    PFunc p = CheckResult;
+
+    DiceRoll(p);
+
+    return 0;
+}
